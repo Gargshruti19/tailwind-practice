505054240f8c30964c82341c2527c6a830d89351
shruti garg <hellocoders8213@gmail.com>
2024-02-17 17:54:07 +0530

    project deployed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["x"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				main: "url('./assests/image1.webp')",
+			},
+		},
+	},
+	plugins: [],
+};
